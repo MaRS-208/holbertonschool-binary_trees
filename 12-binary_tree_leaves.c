27cd546bb53902c766320b401138a11412333776
MaRS-208 <manuela.riossosa@gmail.com>
2022-07-19 17:21:10 -0300

    12. Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,9 +13,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left != NULL)
+	if (tree->left)
 		c += binary_tree_leaves(tree->left);
-	if (tree->right != NULL)
+	if (tree->right)
 		c += binary_tree_leaves(tree->right);
 	return (c);
 }
