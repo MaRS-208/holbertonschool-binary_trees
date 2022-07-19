c30828e793114e1c0bf3235ed2b3b43ad9a28876
MaRS-208 <manuela.riossosa@gmail.com>
2022-07-19 17:32:57 -0300

    13. Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,5 +7,17 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	
+	size_t c = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left || tree->right)
+	{
+		if (tree->left)
+			c += binary_tree_nodes(tree->left);
+		if (tree->right)
+			c += binary_tree_nodes(tree->right);
+		return (c + 1);
+	}
+	return (c);
 }
